2bfc0ce19d0b9e935210ea922de3ae618dcdc605
zhengjun <565061763@qq.com>
2022-06-12 22:14:22 +0800

    FExceptionHttp

--- lib/src/main/java/com/sd/lib/result/exception/FExceptionHttp.kt
+++ lib/src/main/java/com/sd/lib/result/exception/FExceptionHttp.kt
@@ -1,9 +1,38 @@
 package com.sd.lib.result.exception
 
+import android.content.Context
+import com.sd.lib.result.R
+import java.net.SocketTimeoutException
+
 /**
  * Http异常
  */
 open class FExceptionHttp @JvmOverloads constructor(
     message: String? = "",
     cause: Throwable? = null,
-) : FException(message, cause)
+) : FException(message, cause) {
+
+    /** 错误描述 */
+    open fun getDescFormat(context: Context): String {
+        return when (cause) {
+            is SocketTimeoutException -> context.getString(R.string.lib_result_http_desc_exception_timeout)
+            else -> context.getString(R.string.lib_result_http_desc_exception_http)
+        } + toStringAppend()
+    }
+
+    protected fun toStringAppend(): String {
+        val content = toString()
+        return if (content.isEmpty()) {
+            content
+        } else {
+            "，${content}"
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun wrap(e: Throwable): FExceptionHttp {
+            return if (e is FExceptionHttp) e else FExceptionHttp(null, e)
+        }
+    }
+}
