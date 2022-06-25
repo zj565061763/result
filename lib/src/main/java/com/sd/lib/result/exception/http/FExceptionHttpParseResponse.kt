06d73a5528950c9f8c1638b5f4c247d61c938eee
zhengjun <565061763@qq.com>
2022-06-25 16:19:54 +0800

    FExceptionHttpParseResponse

--- lib/src/main/java/com/sd/lib/result/exception/http/FExceptionHttpParseResponse.kt
+++ lib/src/main/java/com/sd/lib/result/exception/http/FExceptionHttpParseResponse.kt
@@ -7,22 +7,13 @@ import com.sd.lib.result.ext.LibContentProvider
  * Http返回数据解析异常
  */
 class FExceptionHttpParseResponse @JvmOverloads constructor(
-    message: String? = "",
+    message: String? = null,
     cause: Throwable? = null,
 ) : FExceptionHttp(message, cause) {
 
-    override val formatMessage: String
+    override val formatCause: String
         get() {
-            val context = LibContentProvider.application ?: return super.formatMessage
-            return buildString {
-                val message = localizedMessage ?: context.getString(R.string.lib_result_http_desc_exception_http)
-                val causeInfo = context.getString(R.string.lib_result_http_desc_exception_parse_response)
-
-                append(message)
-                if (message.isNotEmpty() && causeInfo.isNotEmpty()) {
-                    append(" ")
-                }
-                append(causeInfo)
-            }
+            val context = LibContentProvider.application ?: return super.formatCause
+            return context.getString(R.string.lib_result_http_desc_exception_parse_response)
         }
 }
