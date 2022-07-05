0bd4f030aea59f9cc1990de47a4c58f8dbfb73e6
zhengjun <565061763@qq.com>
2022-07-05 21:43:18 +0800

    FResultCallback

--- lib/src/main/java/com/sd/lib/result/ext/FResultCallback.kt
+++ lib/src/main/java/com/sd/lib/result/ext/FResultCallback.kt
@@ -2,7 +2,7 @@ package com.sd.lib.result.ext
 
 import com.sd.lib.result.FResult
 
-interface FResultCallback<T> {
+fun interface FResultCallback<T> {
     fun onResult(result: FResult<T>)
 
     fun interface Cancelable {
