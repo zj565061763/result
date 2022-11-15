1ec29e4eaae872fa1ef869543828f0270d9d8fbd
565061763 <565061763@qq.com>
2022-11-16 03:54:19 +0800

    demo

--- app/src/main/java/com/sd/demo/result/MainActivity.kt
+++ app/src/main/java/com/sd/demo/result/MainActivity.kt
@@ -11,7 +11,6 @@ import com.sd.lib.result.exception.http.FExceptionHttpResponseCode
 import java.net.SocketTimeoutException
 
 class MainActivity : AppCompatActivity() {
-    val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,58 +18,60 @@ class MainActivity : AppCompatActivity() {
         testExceptionDesc()
 
         getResult(100).onSuccess {
-            Log.i(TAG, "100 onSuccess:${it}")
+            logMsg { "100 onSuccess:${it}" }
         }
         getResult(100).onFailure {
-            Log.i(TAG, "100 onFailure:${it}")
+            logMsg { "100 onFailure:${it}" }
         }
 
         getResult(0).onSuccess {
-            Log.i(TAG, "0 onSuccess:${it}")
+            logMsg { "0 onSuccess:${it}" }
         }
         getResult(0).onFailure {
-            Log.i(TAG, "0 onFailure:${it}")
+            logMsg { "0 onFailure:${it}" }
         }
 
         val loadingResult = FResult.loading("loading")
-        Log.i(TAG, "isLoading:${loadingResult.isLoading()}")
+        logMsg { "isLoading:${loadingResult.isLoading()}" }
 
         val result100 = getResult(100)
         if (result100.isSuccess()) {
-            Log.i(TAG, "isSuccess:${result100.data}")
+            logMsg { "isSuccess:${result100.data}" }
         } else {
-            Log.i(TAG, "isFailure:${result100.exception}")
+            logMsg { "isFailure:${result100.exception}" }
         }
 
         val result0 = getResult(0)
         if (result0.isFailure()) {
-            Log.i(TAG, "isFailure:${result0.exception}")
+            logMsg { "isFailure:${result0.exception}" }
         } else {
-            Log.i(TAG, "isSuccess:${result0.data}")
+            logMsg { "isSuccess:${result0.data}" }
         }
 
         runCatching {
             "success"
         }.toFResult().onSuccess {
-            Log.i(TAG, "toFResult onSuccess:${it}")
+            logMsg { "toFResult onSuccess:${it}" }
         }
 
         runCatching {
             throw RuntimeException("error")
         }.toFResult().onFailure {
-            Log.i(TAG, "toFResult onFailure:${it}")
+            logMsg { "toFResult onFailure:${it}" }
         }
     }
 
     private fun testExceptionDesc() {
-        Log.i(TAG, "1 " + FException().toString())
-        Log.i(TAG, "2 " + FException(message = "error", cause = RuntimeException("runtime")).toString())
-        Log.i(TAG, "--------------------")
-        Log.i(TAG, "1 " + FExceptionHttp().toString())
-        Log.i(TAG, "2 " + FExceptionHttp(message = "error", cause = RuntimeException("runtime")).toString())
-        Log.i(TAG, "3 " + FExceptionHttp(cause = SocketTimeoutException()).toString())
-        Log.i(TAG, "4 " + FExceptionHttpResponseCode(404).toString())
-        Log.i(TAG, "5 " + FExceptionHttpParseResponse().toString())
+        logMsg { "1 " + FException().toString() }
+        logMsg { "2 " + FException(message = "error", cause = RuntimeException("runtime")).toString() }
+
+        logMsg { "--------------------" }
+
+        logMsg { "1 " + FExceptionHttp().toString() }
+        logMsg { "2 " + FExceptionHttp(message = "error", cause = RuntimeException("runtime")).toString() }
+        logMsg { "3 " + FExceptionHttp(cause = SocketTimeoutException()).toString() }
+        logMsg { "4 " + FExceptionHttpResponseCode(404).toString() }
+        logMsg { "5 " + FExceptionHttpParseResponse().toString() }
     }
 
     companion object {
@@ -82,4 +83,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+}
+
+fun logMsg(block: () -> String) {
+    Log.i("FResult-demo", block())
 }
