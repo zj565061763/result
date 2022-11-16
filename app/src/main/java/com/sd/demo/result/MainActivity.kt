088f00b7e52e8d3d65302c54f2c40b1ad6e60ded
565061763 <565061763@qq.com>
2022-11-16 11:42:53 +0800

    demo

--- app/src/main/java/com/sd/demo/result/MainActivity.kt
+++ app/src/main/java/com/sd/demo/result/MainActivity.kt
@@ -3,12 +3,10 @@ package com.sd.demo.result
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.sd.lib.result.FResult
 import com.sd.lib.result.exception.FException
 import com.sd.lib.result.exception.http.FExceptionHttp
 import com.sd.lib.result.exception.http.FExceptionHttpParseResponse
 import com.sd.lib.result.exception.http.FExceptionHttpResponseCode
-import com.sd.lib.result.toFResult
 import java.net.SocketTimeoutException
 
 class MainActivity : AppCompatActivity() {
@@ -17,41 +15,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         testExceptionDesc()
-
-        runCatching {
-            "success"
-        }.toFResult().onSuccess {
-            logMsg { "toFResult onSuccess:${it}" }
-        }
-
-        runCatching {
-            throw RuntimeException("error")
-        }.toFResult().onFailure {
-            logMsg { "toFResult onFailure:${it}" }
-        }
     }
 
     private fun testExceptionDesc() {
-        logMsg { "1 " + FException().toString() }
-        logMsg { "2 " + FException(message = "error", cause = RuntimeException("runtime")).toString() }
+        logMsg { "1 ${FException()}" }
+        logMsg { "2 ${FException(message = "error", cause = RuntimeException("runtime"))}" }
 
         logMsg { "--------------------" }
 
-        logMsg { "1 " + FExceptionHttp().toString() }
-        logMsg { "2 " + FExceptionHttp(message = "error", cause = RuntimeException("runtime")).toString() }
-        logMsg { "3 " + FExceptionHttp(cause = SocketTimeoutException()).toString() }
-        logMsg { "4 " + FExceptionHttpResponseCode(404).toString() }
-        logMsg { "5 " + FExceptionHttpParseResponse().toString() }
-    }
-
-    companion object {
-        private fun getResult(value: Int): FResult<Int> {
-            return if (value == 100) {
-                FResult.success(value)
-            } else {
-                FResult.failure("error")
-            }
-        }
+        logMsg { "1 ${FExceptionHttp()}" }
+        logMsg { "2 ${FExceptionHttp(message = "error", cause = RuntimeException("runtime"))}" }
+        logMsg { "3 ${FExceptionHttp(cause = SocketTimeoutException())}" }
+        logMsg { "4 ${FExceptionHttpResponseCode(404)}" }
+        logMsg { "5 ${FExceptionHttpParseResponse()}" }
     }
 }
 
