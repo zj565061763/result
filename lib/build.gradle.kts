2acfd968be42858b771556d8479bd7eaacdb353d
zj565061763 <565061763@qq.com>
2023-01-23 11:14:56 +0800

    kts

---
+++ lib/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    `maven-publish`
+}
+
+val libGroupId = "com.sd.lib.android"
+val libArtifactId = "result"
+val libVersionName = "1.2.1"
+
+android {
+    namespace = "com.sd.lib.result"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        kotlinOptions.freeCompilerArgs += listOf("-module-name", "$libGroupId.$libArtifactId")
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+dependencies {
+    implementation("com.github.zj565061763:ctx:1.0.0")
+}
+
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = libGroupId
+                artifactId = libArtifactId
+                version = libVersionName
+            }
+        }
+    }
+}
