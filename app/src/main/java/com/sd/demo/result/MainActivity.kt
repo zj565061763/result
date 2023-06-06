66c9ea3363a3ea3bfb4cbcc900da1f1271943ab8
zj565061763 <565061763@qq.com>
2023-06-06 17:18:54 +0800

    demo

--- app/src/main/java/com/sd/demo/result/MainActivity.kt
+++ app/src/main/java/com/sd/demo/result/MainActivity.kt
@@ -2,58 +2,18 @@ package com.sd.demo.result
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.sd.demo.result.ui.theme.AppTheme
+import androidx.appcompat.app.AppCompatActivity
 import com.sd.lib.result.exception.FException
 import com.sd.lib.result.exception.http.FExceptionHttp
 import com.sd.lib.result.exception.http.FExceptionHttpParseResponse
 import com.sd.lib.result.exception.http.FExceptionHttpResponseCode
 import java.net.SocketTimeoutException
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AppTheme {
-                Content()
-            }
-        }
-    }
-}
-
-@Composable
-private fun Content() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(5.dp)
-    ) {
-        Button(
-            onClick = {
-                testExceptionDesc()
-            }
-        ) {
-            Text(text = "button")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun DefaultPreview() {
-    AppTheme {
-        Content()
+        setContentView(R.layout.activity_main)
+        testExceptionDesc()
     }
 }
 
